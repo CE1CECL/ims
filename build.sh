0834fc32bc06d9080acb193bec3fb675cb2ac782
Christopher Lentocha <christopherericlentocha@gmail.com>
2026-01-17 14:39:29 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
-set -e
-
-if [ -d /usr/lib/jvm/java-8-openjdk-amd64/bin/ ];then
-export PATH=/usr/lib/jvm/java-8-openjdk-amd64/bin/:$PATH
-fi
+set -x
 
 if [ -z "$ANDROID_HOME" ];then
-    export ANDROID_HOME=$PWD/sdk
+export ANDROID_HOME=/usr/lib/android-sdk
 fi
+
+zip -d $ANDROID_HOME/platforms/android-33/android.jar android/telephony/ims/feature/MmTelFeature.class android/telephony/ims/feature/MmTelFeature\$MmTelCapabilities.class
 
 gradleTarget=assembleDebug
 target=debug
 file=app-debug
-if [ "$1" == "release" ];then
-    gradleTarget=assembleRelease
-    target=release
-    file=app-release-unsigned
+if [ "$1" == "release" ]; then
+gradleTarget=assembleRelease
+target=release
+file=app-release-unsigned
 fi
 ./gradlew $gradleTarget
 LD_LIBRARY_PATH=./signapk/ java -jar signapk/signapk.jar keys/platform.x509.pem keys/platform.pk8 ./app/build/outputs/apk/$target/${file}.apk app.apk
